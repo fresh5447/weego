5a38a1e573910acdb190b881f087ba0a510bb618
cposey-firelab <cody.posey85@gmail.com>
2014-03-29 16:59:09 -0600

    initial commit

---
+++ Scripts/_references.js
@@ -0,0 +1,5 @@
+﻿/// <reference path="jquery-ui-1.10.4.js" />
+/// <reference path="jquery-2.1.0.js" />
+/// <reference path="bootstrap.js" />
+/// <reference path="underscore.js" />
+/// <reference path="bootstrap.js" />
---
+++ js/Backbone/weeGo.js
@@ -0,0 +1,43 @@
+﻿
+    var CreateEventModel = Backbone.Model.extend({
+        defaults: function () {
+            return {
+                Category: ['Basketball', 'Rock Climbing', 'Floating'],
+                Location: "Nowhere",
+                Time: new Date(),
+                MinimumAttendees: 0,
+                MaximumAttendees: 9999,
+                Description: "No description"
+            };
+        }
+    });
+
+    var CreateEventView = Backbone.View.extend({
+        model: CreateEventModel,
+
+        el: $('#createEvent'),
+
+        initialize: function () {
+            _.bindAll(this, "render");
+            this.model = new CreateEventModel();
+            this.template = _.template($('#createEvent-view-template').html());
+            this.render();
+        },
+
+        render: function () {
+            this.$el.append(this.template(this.model.toJSON()));
+            return this;
+        }
+    });
+
+    var CreateEventButtonView = Backbone.View.extend({
+        el: $('#createEventBtn'),
+
+        events: {
+            'click': 'createEvent'
+        },
+
+        createEvent: function(){
+            var createEvent = new CreateEventView();
+        }
+    });
